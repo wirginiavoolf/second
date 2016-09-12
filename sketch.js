755dd5c7f7e1f44d2187d989734003a7cffc5075
wirginiavoolf <stephzanie@gmail.com>
2016-09-11 20:40:37 -0400

    Add files

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var fillColor = 0;
+
+function setup() {
+  
+}
+
+//words show up in sequence, but stay printed: 
+/*
+function mousePressed(){
+textSize(32);
+text("if", 10, 30);
+fill=0;
+  
+}
+
+function mouseReleased(){
+textSize(32);
+text("than", 10, 30);
+fill=0;
+  
+}
+*/
+
+
+//words show up simultaneously:
+/*
+
+function mouseClicked() {
+  {
+    textSize(32);
+    text("if", 10, 30);
+    console.log("if");
+  } 
+}
+function mouseReleased() {  
+  {
+  textSize(32);
+    text("else", 10, 30);
+    console.log("else");
+  }
+]
+
+
+*/
+
